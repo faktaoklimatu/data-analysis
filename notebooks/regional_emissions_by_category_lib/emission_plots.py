984f251bec50d77b9757c971bff66b385dcb03d1
Matěj <piromatej@seznam.cz>
2022-12-01 18:53:29 +0100

    Split code from jupyter notebook to python files

---
+++ notebooks/regional_emissions_by_category_lib/emission_plots.py
@@ -0,0 +1,127 @@
+import matplotlib.pyplot as plt
+import eurostat
+
+from regional_emissions_by_category_lib.emission_state_definitions import definitions
+
+def get_definition_dict(state):
+    return definitions[state]
+
+
+def get_data(state, year):
+    df = eurostat.get_data_df('env_air_gge')
+    # Pandas query() does not allow backslash in column names so "rename column" is needed.
+    df = df.rename(columns={'geo\\time': 'geo'})
+    df = df.query("airpol == 'GHG' and geo == @state and unit == 'MIO_T'")
+    df = df[["src_crf", year]]  # jen kod odvětví a rok
+    df = df.set_index('src_crf')  # src_crf jako index
+    df = df.rename(columns={year: 'value'})
+    return df
+
+
+def get_value(key, df):  # získej hodnotu. Zadej klíč a df
+    return df.loc[key, 'value']
+
+
+def get_sum(keys, df):
+    sum = 0
+    for key in keys:
+        sum += get_value(key, df)
+    return sum
+
+
+def add_sums_and_reminder(definition, total_value_code, perc_dict, df):
+    """Computes the sum values and a reminder.
+    perc_dict gathers percents for each wedge.
+    So we can show them in outer chart labels.
+    """
+    cumulative_sum = 0
+    total_divider = get_value('TOTX4_MEMONIA', df)  # dělitel pro výpočet procent
+    for wedge_def in definition:
+        wedge_code = wedge_def['code']
+        if 'sum' in wedge_def:
+            wedge_value = get_sum(wedge_def['sum'], df)
+            cumulative_sum += wedge_value
+        elif 'reminder' in wedge_def:
+            wedge_value = get_value(total_value_code, df) - cumulative_sum
+        perc_dict[wedge_code] = (
+                    wedge_value / total_divider)  # tohle mi počítá proceentuální zastoupení výseče proti celku a hází to hodnoty do listu
+        df.loc[wedge_code, 'value'] = wedge_value  # přidá nový řádek ke stávajímu df
+
+
+def compute_values(definition, df, outer_perc_dict):
+    add_sums_and_reminder(definition, 'TOTX4_MEMONIA', {}, df)
+
+    for wedge_def in definition:
+        if 'breakdown' in wedge_def:
+            add_sums_and_reminder(wedge_def['breakdown'], wedge_def['code'], outer_perc_dict, df)
+
+
+def create_plot_lists(definition, outer_perc_dict):
+    plot_dict = {
+        "inner_chart_structure": [], "outer_chart_structure": [],
+        "inner_labels": [], "outer_labels": [],
+        "inner_colors": [], "outer_colors": []
+    }
+
+    for area in definition:
+        plot_dict["inner_chart_structure"].append(area['code'])
+        plot_dict["inner_labels"].append(area['label'])
+        plot_dict["inner_colors"].append(area['color'])
+        if 'breakdown' not in area:
+            continue
+        for subarea in area['breakdown']:
+            plot_dict["outer_chart_structure"].append(subarea['code'])
+            plot_dict["outer_labels"].append(
+                f"{subarea['label']} {(outer_perc_dict[subarea['code']]):.1%}")  # outer_perc_dict
+            plot_dict["outer_colors"].append(subarea['color'])
+    return plot_dict
+
+
+def create_relative_outer_df(inner_chart_structure, outer_chart_structure, df):
+    total = get_sum(inner_chart_structure, df)
+    return df.loc[outer_chart_structure] / total
+
+
+def draw_plot(state, year, plot_dict, df_rel, df):
+    fig, ax = plt.subplots(figsize=(12, 12))
+    inner_size = 0.35
+    outer_size = 0.2
+
+    # vnitřní
+    ax.pie(df.loc[plot_dict['inner_chart_structure']]['value'], radius=(2 * inner_size),
+           labels=plot_dict['inner_labels'],
+           counterclock=False,
+           startangle=90, colors=plot_dict['inner_colors'], autopct='%1.1f%%',
+           textprops={'fontsize': 12, 'fontweight': 'bold', 'color': '#000000'}, pctdistance=0.73,
+           labeldistance=1.55,
+           wedgeprops=dict(width=inner_size, edgecolor='w'))
+
+    # vnější
+    ax.pie(df_rel['value'], radius=(2 * inner_size + outer_size), labels=plot_dict['outer_labels'], counterclock=False,
+           startangle=90, normalize=False, colors=plot_dict['outer_colors'],
+           textprops={'fontsize': 12}, labeldistance=1.2,
+           wedgeprops=dict(width=outer_size, edgecolor='w'))
+
+    # nadpis
+    plt.title(f'Emise skleníkových plynů pro {state} za rok {year} v CO2 ekviv.', fontsize=20)
+
+    # číslo v prostřed
+    total_emisions = round(df.loc['TOTX4_MEMONIA', 'value'], 2)
+    ax.annotate(total_emisions, xy=(0.1, 0.1), xytext=(-0.15, -0.01), fontsize=25)
+
+    plt.show()
+
+
+def create_plot(state, year):
+    definition = get_definition_dict(state)
+    df = get_data(state, year)
+
+    outer_perc_dict = {}
+    compute_values(definition, df, outer_perc_dict)
+
+    plot_dict = create_plot_lists(definition, outer_perc_dict)
+
+    df_rel = create_relative_outer_df(plot_dict['inner_chart_structure'],
+                                      plot_dict['outer_chart_structure'], df)
+
+    draw_plot(state, year, plot_dict, df_rel, df)
---
+++ notebooks/regional_emissions_by_category_lib/emission_state_definitions.py
@@ -0,0 +1,174 @@
+definitions = {
+    'SK': [
+        {'code': 'CRF2',  # vnitřní graf, políčko 1                 PRŮMYSLOVÉ PROCESY
+         'label': 'Průmyslové procesy (výroba cementu, oceli...)',
+         'sum': ['CRF2'],
+         'color': '#8A2BE2',
+         'breakdown': [
+             {'code': 'CRF2C',  # vnější graf políčko 1.1
+              'label': 'Zpracování oceli a kovů',
+              'color': '#68228B',
+              'sum': ['CRF2C']},
+             {'code': 'CRF2A',  # vnější graf políčko 1.2
+              'label': 'Minerální produkce (cement, vápno...)',
+              'color': '#9A32CD',
+              'sum': ['CRF2A']},
+             {'code': 'CRF2B',  # vnější graf políčko 1.3
+              'label': 'Chemický průmysl',
+              'color': '#B23AEE',
+              'sum': ['CRF2B']},
+             {'code': 'CRF2_dif',  # vnější graf políčko 1.3 - rozdíl
+              'label': 'Další průmyslové procesy',
+              'color': '#BF3EFF',
+              'reminder': True}]},
+        {'code': 'CRF1A3_CRF1D1A',  # vnitřní graf, políčko 2         DOPRAVA
+         'label': 'Doprava (včetně letecké)',
+         'sum': ['CRF1A3', 'CRF1D1A'],
+         'color': '#B22222',
+         'breakdown': [
+             {'code': 'CRF1A3B1',  # vnější graf políčko 2.1
+              'label': 'Osobní automobilová doprava',
+              'color': '#8B1A1A',
+              'sum': ['CRF1A3B1']},
+             {'code': 'CRF1A3B2_CRF1A3B3',  # vnější graf políčko 2.2
+              'label': 'Nákladní a autobusová doprava',
+              'color': '#CD2626',
+              'sum': ['CRF1A3B2', 'CRF1A3B3']},
+             {'code': 'CRF1D1A_CRF1A3A',  # vnější graf políčko 2.3
+              'label': 'Letecká doprava',
+              'color': '#EE2C2C',
+              'sum': ['CRF1D1A', 'CRF1A3A']},
+             {'code': 'CRF1A3_dif',  # vnější graf políčko 2.4 - rozdíl
+              'label': 'Jiná doprava',
+              'color': '#FF3030',
+              'reminder': True}]},
+        {'code': 'CRF1A2',  # vnitřní graf, políčko 3                 SPALOVÁNÍ V PRŮMYSLU
+         'label': 'Spalování v průmyslu',
+         'sum': ['CRF1A2'],
+         'color': '#009ACD',
+         'breakdown': [
+             {'code': 'CRF1A2A_CRF1A2B',  # vnější graf políčko 3.1
+              'label': 'Spalování při zpracování oceli a kovů',
+              'color': '#00688B',
+              'sum': ['CRF1A2A', 'CRF1A2B']},
+             {'code': 'CRF1A2_dil',  # vnější graf políčko 3.2 - rozdíl
+              'label': 'Další spalování v průmyslu',
+              'color': '#00B2EE',
+              'reminder': True}]},
+        {'code': 'CRF1A1',  # vnitřní graf, políčko 4                 ENERGETIKA
+         'label': 'Energetika',
+         'sum': ['CRF1A1'],
+         'color': '#FF4500',
+         'breakdown': [
+             {'code': 'CRF1A1C_CRF1A1B',  # vnější graf políčko 4.1
+              'label': 'Rafinace, výroba a úprava paliv',
+              'color': '#CD3700',
+              'sum': ['CRF1A1C', 'CRF1A1B']},
+             {'code': 'CRF1A2_dif_X',  # vnější graf políčko 4.2 - rozdíl
+              'label': 'Teplárny a elektrárny',
+              'color': '#EE4000',
+              'reminder': True}]},
+        {'code': 'CRF1A4',  # vnitřní graf, políčko 5                 SPALOVÁNÍ V DOM, INST A ZEMĚĎ
+         'label': 'Spalování v domácnostech, institucích a zemědělství',
+         'sum': ['CRF1A4'],
+         'color': '#6495ED'},
+        {'code': 'CRF3',  # vnitřní graf, políčko 6                   ZEMĚDĚLSTVÍ
+         'label': 'Zemědělství',
+         'sum': ['CRF3'],
+         'color': '#48D1CC'},
+        {'code': 'CRF5',  # vnitřní graf, políčko 7                   ODPADOVÉ HOSPODÁŘSTVÍ
+         'label': 'Odpadové hospodářství',
+         'sum': ['CRF5'],
+         'color': '#3CB371'},
+        {'code': 'TOTAL_DIF',  # vnitřní graf, políčko 8                   JINÉ
+         'label': 'Jiné',
+         'color': '#FFD700',
+         'reminder': True}
+    ],
+    'CZ': [
+        {'code': 'CRF2',  # vnitřní graf, políčko 1                 PRŮMYSLOVÉ PROCESY
+         'label': 'Průmyslové procesy (výroba cementu, oceli...)',
+         'sum': ['CRF2'],
+         'color': '#00FFFF',
+         'breakdown': [
+             {'code': 'CRF2C',  # vnější graf políčko 1.1
+              'label': 'Zpracování oceli a kovů',
+              'color': '#68228B',
+              'sum': ['CRF2C']},
+             {'code': 'CRF2A',  # vnější graf políčko 1.2
+              'label': 'Minerální produkce (cement, vápno...)',
+              'color': '#9A32CD',
+              'sum': ['CRF2A']},
+             {'code': 'CRF2B',  # vnější graf políčko 1.3
+              'label': 'Chemický průmysl',
+              'color': '#B23AEE',
+              'sum': ['CRF2B']},
+             {'code': 'CRF2_dif',  # vnější graf políčko 1.3 - rozdíl
+              'label': 'Další průmyslové procesy',
+              'color': '#BF3EFF',
+              'reminder': True}]},
+        {'code': 'CRF1A3_CRF1D1A',  # vnitřní graf, políčko 2         DOPRAVA
+         'label': 'Doprava (včetně letecké)',
+         'sum': ['CRF1A3', 'CRF1D1A'],
+         'color': '#B22222',
+         'breakdown': [
+             {'code': 'CRF1A3B1',  # vnější graf políčko 2.1
+              'label': 'Osobní automobilová doprava',
+              'color': '#8B1A1A',
+              'sum': ['CRF1A3B1']},
+             {'code': 'CRF1A3B2_CRF1A3B3',  # vnější graf políčko 2.2
+              'label': 'Nákladní a autobusová doprava',
+              'color': '#CD2626',
+              'sum': ['CRF1A3B2', 'CRF1A3B3']},
+             {'code': 'CRF1D1A_CRF1A3A',  # vnější graf políčko 2.3
+              'label': 'Letecká doprava',
+              'color': '#EE2C2C',
+              'sum': ['CRF1D1A', 'CRF1A3A']},
+             {'code': 'CRF1A3_dif',  # vnější graf políčko 2.4 - rozdíl
+              'label': 'Jiná doprava',
+              'color': '#FF3030',
+              'reminder': True}]},
+        {'code': 'CRF1A2',  # vnitřní graf, políčko 3                 SPALOVÁNÍ V PRŮMYSLU
+         'label': 'Spalování v průmyslu',
+         'sum': ['CRF1A2'],
+         'color': '#009ACD',
+         'breakdown': [
+             {'code': 'CRF1A2A_CRF1A2B',  # vnější graf políčko 3.1
+              'label': 'Spalování při zpracování oceli a kovů',
+              'color': '#00688B',
+              'sum': ['CRF1A2A', 'CRF1A2B']},
+             {'code': 'CRF1A2_dil',  # vnější graf políčko 3.2 - rozdíl
+              'label': 'Další spalování v průmyslu',
+              'color': '#00B2EE',
+              'reminder': True}]},
+        {'code': 'CRF1A1',  # vnitřní graf, políčko 4                 ENERGETIKA
+         'label': 'Energetika',
+         'sum': ['CRF1A1'],
+         'color': '#FF4500',
+         'breakdown': [
+             {'code': 'CRF1A1C_CRF1A1B',  # vnější graf políčko 4.1
+              'label': 'Rafinace, výroba a úprava paliv',
+              'color': '#CD3700',
+              'sum': ['CRF1A1C', 'CRF1A1B']},
+             {'code': 'CRF1A2_dif_X',  # vnější graf políčko 4.2 - rozdíl
+              'label': 'Teplárny a elektrárny',
+              'color': '#EE4000',
+              'reminder': True}]},
+        {'code': 'CRF1A4',  # vnitřní graf, políčko 5                 SPALOVÁNÍ V DOM, INST A ZEMĚĎ
+         'label': 'Spalování v domácnostech, institucích a zemědělství',
+         'sum': ['CRF1A4'],
+         'color': '#6495ED'},
+        {'code': 'CRF3',  # vnitřní graf, políčko 6                   ZEMĚDĚLSTVÍ
+         'label': 'Zemědělství',
+         'sum': ['CRF3'],
+         'color': '#48D1CC'},
+        {'code': 'CRF5',  # vnitřní graf, políčko 7                   ODPADOVÉ HOSPODÁŘSTVÍ
+         'label': 'Odpadové hospodářství',
+         'sum': ['CRF5'],
+         'color': '#3CB371'},
+        {'code': 'TOTAL_DIF',  # vnitřní graf, políčko 8                   JINÉ
+         'label': 'Jiné',
+         'color': '#FFD700',
+         'reminder': True}
+    ],
+}
