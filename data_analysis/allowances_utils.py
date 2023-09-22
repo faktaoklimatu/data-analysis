148c2d61e06b609bd3d7fc3b0d16da6d3ca2ee3e
Jan Krčál <jan.krcal@faktaoklimatu.cz>
2023-09-22 11:16:38 +0200

    Exploration of EUA industry for SK + minor polish

--- data_analysis/allowances_utils.py
+++ data_analysis/allowances_utils.py
@@ -1,22 +1,29 @@
 """ Utils to load allowances data (EUA) from an Excel file. """
 
+from typing import Optional
+
 import pandas as pd
 
 
 _HEADER_LINE_START = 16  # global variable
 
 
-def get_allowances_data(year: int, registry_code: str, main_activity_code: int, eua_path: str) -> pd.DataFrame:
+def get_allowances_data(year: int, registry_code: str,
+                        main_activity_code: Optional[int], eua_path: str) -> pd.DataFrame:
     """
-    Import allowances data from an excel file for a given year and given list of permit codes.
-    The dataframe is indexed by permit codes and has a single column, 'value', in megatons CO2.
+    Import allowances data from an excel file for a given year and optinally, given code of main
+    activity. The dataframe is indexed by permit codes and has a single column, 'value', in megatons
+    CO2.
     """
     df: pd.DataFrame = pd.read_excel(eua_path, header=_HEADER_LINE_START)
     df = df.rename(
         columns={f"VERIFIED_EMISSIONS_{year}": "value"})
-    df = df.loc[((df["MAIN_ACTIVITY_TYPE_CODE"] == main_activity_code) &
-                 (df["REGISTRY_CODE"] == registry_code)),
-                ["PERMIT_IDENTIFIER", "value", "IDENTIFIER_IN_REG"]]
+
+    main_activity_code_condition = True
+    if main_activity_code is not None:
+        main_activity_code_condition = (df["MAIN_ACTIVITY_TYPE_CODE"] == main_activity_code)
+    df = df.loc[(main_activity_code_condition & (df["REGISTRY_CODE"] == registry_code)),
+                ["PERMIT_IDENTIFIER", "value", "IDENTIFIER_IN_REG", "MAIN_ACTIVITY_TYPE_CODE"]]
     df.set_index('PERMIT_IDENTIFIER', inplace=True)
     # Some elements are string "Excluded" or -1, convert that to zeros. Convert tons to megatons.
     df["value"] = pd.to_numeric(df["value"],
