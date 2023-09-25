08c84ceb9c669c65c0d0de11493d09e183e28def
Matěj Grabovský <matej@mgrabovsky.net>
2023-09-25 17:08:39 +0200

    Factor out common emission cats. calculation
    - Factor out common R code for calculating our GHG emission categories
      into a custom library.
    - Reuse the library in the selected countries notebook and the emissions
      over time notebook.

---
+++ lib/R/emissions-utils.r
@@ -0,0 +1,21 @@
+#' Calculate emission categories from CRF data
+#'
+#' @export
+fakta_calculate_emission_categories <- function(.data) {
+  # Pivot to a wide tibble for easier calculations.
+  pivot_wider(.data, names_from = "Code", values_from = "Value") |>
+    # Group into sectors by summing components.
+    mutate(PowerHeat   = CRF1A1A,
+           Industry    = CRF1A2 + CRF1A1B + CRF1A1C + CRF1A3E + CRF2 + CRF1B,
+           Transport   = CRF1A3A + CRF1A3B + CRF1A3C + CRF1A3D + CRF1D1A,
+           Buildings   = CRF1A4A + CRF1A4B,
+           Agriculture = CRF3 + CRF1A4C,
+           Waste       = CRF5,
+           Other       = TOTX4_MEMONIA - PowerHeat - Industry - Transport -
+            Buildings - Agriculture - Waste) |>
+    select(!(starts_with("CRF") | starts_with("TOTX"))) |>
+    # Turn back into long format.
+    pivot_longer(PowerHeat:Other,
+                 names_to = "Category",
+                 values_to = "Value")
+}
