febd4a38f41598b5395e4eda0b1c4d18596ef12e
Matěj Kolouch Grabovský <matej@mgrabovsky.eu>
2025-09-15 16:09:49 +0200

    Update selected country emissions for 2023

--- lib/R/emissions-utils.r
+++ lib/R/emissions-utils.r
@@ -5,29 +5,34 @@ fakta_calculate_emission_categories <- function(.data) {
   # Pivot to a wide tibble for easier calculations.
   pivot_wider(.data, names_from = "Code", values_from = "Value") |>
     # Group into sectors by summing components.
-    mutate(PowerHeat   = CRF1A1A,
-           Industry    = CRF1A2 + CRF1A1B + CRF1A1C + CRF1A3E + CRF2 + CRF1B,
-           Transport   = CRF1A3A + CRF1A3B + CRF1A3C + CRF1A3D + CRF1D1A,
-           Buildings   = CRF1A4A + CRF1A4B,
-           Agriculture = CRF3 + CRF1A4C,
-           Waste       = CRF5,
-           Other       = TOTX4_MEMO + CRF1D1A - PowerHeat - Industry - Transport -
-            Buildings - Agriculture - Waste) |>
+      PowerHeat = CRF1A1A,
+      Industry = CRF1A2 + CRF1A1B + CRF1A1C + CRF1A3E + CRF2 + CRF1B,
+      Transport = CRF1A3A + CRF1A3B + CRF1A3C + CRF1A3D + CRF1D1A,
+      Buildings = CRF1A4A + CRF1A4B,
+      Agriculture = CRF3 + CRF1A4C,
+      Waste = CRF5,
+      Other = TOTX4_MEMO + CRF1D1A - PowerHeat - Industry - Transport -
+        Buildings - Agriculture - Waste
+    ) |>
     select(!(starts_with("CRF") | starts_with("TOTX"))) |>
     # Turn back into long format.
-    pivot_longer(PowerHeat:Other,
-                 names_to = "Category",
-                 values_to = "Value")
+    pivot_longer(
+      PowerHeat:Other,
+      names_to = "Category",
+      values_to = "Value"
+    )
 }
 
 #' Colour scheme for GHG emission sectors
 #'
 #' @export
 emission_category_colours <-
-  c(PowerHeat   = "#ff4245",
+  c(
+    PowerHeat   = "#ff4245",
     Industry    = "#3b3b93",
     Transport   = "#8546af",
     Buildings   = "#0d80d8",
     Agriculture = "#1bb0a3",
     Waste       = "#fab519",
-    Other       = "#b5b8bd")
+    Other       = "#b5b8bd"
+  )
