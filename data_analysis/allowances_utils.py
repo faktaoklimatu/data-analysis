cbb287d7fdd6f31e7497c6d1681b05d009f8ddeb
Matěj Kolouch Grabovský <matej@mgrabovsky.eu>
2025-09-17 11:38:34 +0200

    Add basic error checking

--- data_analysis/allowances_utils.py
+++ data_analysis/allowances_utils.py
@@ -21,15 +21,17 @@ def get_allowances_data(year: int, registry_code: str,
     activity. The dataframe is indexed by permit codes and has a single column, 'value', in megatons
     CO2.
     """
+    emissions_column = f"VERIFIED_EMISSIONS_{year}"
+
     # First pass to find the header row.
     df_test = pd.read_excel(eua_path, header=None, nrows=50)
     skip_rows = _find_first_row(df_test)
 
     # Second pass to read the whole file.
-    df = (
-        pd.read_excel(eua_path, skiprows=skip_rows)
-        .rename(columns={f"VERIFIED_EMISSIONS_{year}": "value"})
-    )
+    df = pd.read_excel(eua_path, skiprows=skip_rows)
+    if emissions_column not in df.columns:
+        raise KeyError(f"Verified emissions for {year} not present in dataset")
+    df = df.rename(columns={emissions_column: "value"})
 
     main_activity_code_condition = True
     if main_activity_code is not None:
@@ -38,6 +40,6 @@ def get_allowances_data(year: int, registry_code: str,
                 ["PERMIT_IDENTIFIER", "value", "IDENTIFIER_IN_REG", "MAIN_ACTIVITY_TYPE_CODE"]]
     df.set_index('PERMIT_IDENTIFIER', inplace=True)
     # Some elements are string "Excluded" or -1, convert that to zeros. Convert tons to megatons.
-    df["value"] = pd.to_numeric(df["value"], errors="coerce").fillna(0).clip(lower=0) / 1_000_000
+    df["value"] = pd.to_numeric(df["value"], errors="coerce").fillna(0).clip(lower=0).div(1e6)
     df.sort_values("value", ascending=False, inplace=True)
     return df
